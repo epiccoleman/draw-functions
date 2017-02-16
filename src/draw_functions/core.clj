1bc8223a51a282f219074ac27e9b50f376446568
Eric Coleman <eric@epiccoleman.com>
2017-02-15 20:56:11 -0500

    refactor to allow drawing of arbitrary function

--- src/draw_functions/core.clj
+++ src/draw_functions/core.clj
@@ -2,15 +2,17 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(defn mod-color [ n ] (rem n 256))
+
 (defn draw [state]
   (q/background 0 0 0)
 
   (doseq [ x (range 500)
            y (range 500) ] 
-  (q/stroke (rem (bit-xor x y) 256))
-  (q/point x y)
-  
-  )
+
+  (q/stroke (mod-color (bit-xor x y)))
+  (q/point x y))
+
 )
 
 (q/defsketch draw-funcs
